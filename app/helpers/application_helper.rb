fe9484e0f824076f2166b68dc00a7c6c424fd018
Jalen Jackson <jalenjack201@hotmail.com>
2017-04-17 03:48:27 -0400

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def checkLine(line)
+      if line =~ /Perl|Python/
+        return line
+      else
+        return ''
+      end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+
+30.times do
+  Product.create(title: 'A new product', description: 'Check me out',image_url: 'cool.jpg', price: 23.40)
+end
