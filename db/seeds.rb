f25731f87078a305106b4210c03aea09a8370ae5
Jalen Jackson <jalenjack201@hotmail.com>
2017-04-17 05:23:33 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 Product.delete_all
 
-
+i = 0
 30.times do
-  Product.create(title: 'A new product', description: 'Check me out',image_url: 'cool.jpg', price: 23.40)
+  i += 1
+  Product.create(title: "A new product #{i}", description: 'Check me out',image_url: 'cool.jpg', price: 23.40)
 end
